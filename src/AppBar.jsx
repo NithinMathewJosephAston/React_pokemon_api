33c868cb026be02b66861b8de2c8880a5315fe7e
Nithin <nj9@sanger.ac.uk>
2025-05-15 14:34:23 +0100

    Navbar font fixtures

--- src/AppBar.jsx
+++ src/AppBar.jsx
@@ -1,29 +1,10 @@
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  width: '100%',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
+import './App.css'
 
 export default function SearchAppBar() {
   return (
@@ -34,20 +15,19 @@ export default function SearchAppBar() {
             size="large"
             edge="start"
             color="inherit"
-            aria-label="open drawer"
+            aria-label="logo"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+            <img
+                src="/images/psyduck.png"
+                alt="Psyduck"
+                style={{ width: 30, height: 30 }}
+            />
           </IconButton>
           <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' },
-            mr: 2,
-            }}
+            className='pixel-font'
           >
-            Psyduck
+            psyduck
           </Typography>
         </Toolbar>
       </AppBar>
